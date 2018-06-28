77c34e879345093980fab299ea948912295a5cfd
PierreDV <p.verteuil@gmail.com>
2018-06-28 20:08:55 +0200

    added basic user model, routes and controller action to build upon

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe '#index' do
+    it 'returns the correct status code' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+end
